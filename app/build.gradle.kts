e31177b91194696db634846eb4d2976a66224cb4
Martin Villena <martin.villena2003@gmail.com>
2025-11-16 14:25:56 -0300

    VitalarmApp version 0.2.162

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "0.2.160"
+        versionName = "0.2.162"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {

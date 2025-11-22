aa0a7b59e5ba61bcba4ffa7d5b3003ec52f9ac18
Martin Villena <martin.villena2003@gmail.com>
2025-11-22 15:38:24 -0300

    VitalarmApp version 0.6.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,9 +12,9 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "0.6.12"
+        versionName = "0.6.13"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        buildConfigField("String", "LIBREAPI_TOKEN", "demo")
+        buildConfigField("String", "LIBREAPI_TOKEN", "\"demo\"")
     }
     buildTypes {
         release {

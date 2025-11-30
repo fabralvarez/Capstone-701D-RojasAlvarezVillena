e2a78340c98abe3b7f1eb56b45df8c60eedefedc
Martin Villena <martin.villena2003@gmail.com>
2025-11-29 23:25:23 -0300

    VitalarmApp version 0.8.664

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "0.8.662"
+        versionName = "0.8.664"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         val localProperties = Properties().apply {

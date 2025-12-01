ef14374fc371019a826204ca20edcf4ea52e4a67
Martin Villena <87401111+martinv6@users.noreply.github.com>
2025-12-01 00:47:54 -0300

    Update versionName to 0.8.800

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "0.8.80"
+        versionName = "0.8.800"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         val localProperties = Properties().apply {

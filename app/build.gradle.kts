1aed234c6e57aa9a25ee4e386f6b3d671f85cf12
Martin Villena <87401111+martinv6@users.noreply.github.com>
2025-11-30 22:00:16 -0300

    Bump app version to 0.8.722

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "0.8.721"
+        versionName = "0.8.722"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         val localProperties = Properties().apply {
